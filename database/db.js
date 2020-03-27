5abd98afaf34d07d54487630302227584095c8e6
Dave Hastings <dhastings725@gmail.com>
2020-03-27 12:27:26 -0400

    changed database password

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
   host     : 'host.docker.internal',
   user     : 'root',
-  password : 'password',
+  password : 'hratx47hratx47',
   database : 'photos'
 });
  
